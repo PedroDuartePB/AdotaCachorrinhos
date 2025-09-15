bf8e3228c98e73a1a11612c1534c4512c9f7e1be
PedroDuarte <pedro.lima@dcx.ufpb.br>
2025-09-15 11:20:46 -0300

    Começo dos trabalhos

---
+++ dados.py
@@ -0,0 +1,7 @@
+def getCachorroLista():
+    with open("perfis.txt", 'r' ) as lista:
+        lista.read()
+
+def cadastraCachorro():
+    with open("perfis.txt", 'r' ) as insert:
+        insert.write()
