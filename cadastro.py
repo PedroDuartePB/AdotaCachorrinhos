15c5705540194667aa07479e6918a9be1c12970b
PedroDuarte <pedro.lima@dcx.ufpb.br>
2025-09-16 13:40:52 -0300

    Recomeçando a estruturação

--- cadastro.py
+++ cadastro.py
@@ -1,14 +1,32 @@
 #estou usando ; como separador nos aquivos 
 #template de perfil dos dog:
-#Nome;ID;Raça;Idade;Sexo
+#ID;Nome;Raça;Idade;Sexo
 
-def getCachorroLista():
+def getDadosCachorro():
     """Apresenta a lista completa dos cachorros disponiveis
     ordenando atraves de categorias"""
     with open("perfis.txt", 'r') as lista:
         content = []
         for linha in lista:
             content.append(linha.strip().split(';'))
-        print(content)
+        return content
 
-getCachorroLista()
+def getId():
+    id = 0
+    with open("perfis.txt", "r") as lista:
+        for lines in lista:
+            id += 1
+    return id+1
+
+
+def cadastraNewCachorro(newDog:list):
+    t = ''
+    if newDog:
+        for dado in newDog:
+            t = t + str(dado).strip()
+        t = t.strip()
+
+    with open("perfis.txt", "a") as lista:
+        lista.write(f"\n{getId()}{t}")
+
+cadastraNewCachorro(["marcio", "caramelo", "2", "M"])
