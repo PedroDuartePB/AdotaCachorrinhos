52acb88bf02d8f27fced72f7f5a79b7ed30d01d0
Pedro H Duarte <pedro.lima@dcx.ufpb.br>
2025-09-24 21:10:04 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -73,7 +73,7 @@ def interfaceAdocao(nome:str=None, raça:str=None, idade:str=None,\
                 if p == None or p == '':
                     busca[key] = ''
                 else:
-                    busca[key] = p
+                    busca[key] = p.lower()
 
             lista = cadastro.getCachorroPerfil(busca['nome'], busca['raça'],
                                                busca['idade'], busca['sexo'], None)
