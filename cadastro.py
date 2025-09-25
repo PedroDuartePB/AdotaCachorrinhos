537f1ce0d1176d65b62b2b5b2baf78cfadc0e3ec
Pedro H Duarte <pedro.lima@dcx.ufpb.br>
2025-09-24 21:10:38 -0300

    Update cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -42,7 +42,7 @@ def getCachorroPerfil(nome:str=None, raça:str=None, idade:str=None,\
                 if nome.lower() in p[1].lower():
                     busca.append(p)
             elif raça:
-                if raça.lower() == p[2].lower():
+                if raça.strip() == p[2].lower():
                     busca.append(p)
             elif idade:
                 if genIdade(p[3]) <= genIdade(idade):
